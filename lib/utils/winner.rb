0907a972a4413976f3c375ff716f4260d30ecfe4
ytkhs <yuta.tkhs@gmail.com>
2018-05-04 23:22:51 +0900

    code format

--- lib/utils/winner.rb
+++ lib/utils/winner.rb
@@ -2,22 +2,22 @@ require 'date'
 require 'json'
 
 class Winner
-	attr_accessor :num, :name, :date
-	
-	def initialize(num, date, name)
-		@num = num.strip.delete("^0-9")
-		@name = name.strip
-		@date = Date.parse(date.strip.gsub('.', '/')) rescue nil
-	end
-	
-	def format(type = 'human')
-		case type
-		when 'csv'
-			[@num, @date&.strftime('%Y-%m-%d'), @name].join(',')
-		when 'json'
-			JSON.pretty_generate(['number' => @num, 'date' => @date&.strftime('%Y-%m-%d'), 'name' => @name])
-		else
-			return sprintf("#%s %s %s", @num.ljust(5), @date&.strftime('%Y-%m-%d'), @name)
-		end
-	end
+  attr_accessor :num, :name, :date
+  
+  def initialize(num, date, name)
+    @num = num.strip.delete("^0-9")
+    @name = name.strip
+    @date = Date.parse(date.strip.gsub('.', '/')) rescue nil
+  end
+  
+  def format(type = 'human')
+    case type
+    when 'csv'
+      [@num, @date&.strftime('%Y-%m-%d'), @name].join(',')
+    when 'json'
+      JSON.pretty_generate(['number' => @num, 'date' => @date&.strftime('%Y-%m-%d'), 'name' => @name])
+    else
+      return sprintf("#%s %s %s", @num.ljust(5), @date&.strftime('%Y-%m-%d'), @name)
+    end
+  end
 end
